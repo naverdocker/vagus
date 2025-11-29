c17c31d3cbf40de2732f94be4121addb7ab7be8e
Naveen Chandar J <naverdocker@gmail.com>
2025-11-29 13:17:25 +0530

    chore(build): scaffold package directory structure and setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="vagus",
+        version="0.1.1"
+        description="The Neural Interface for your Terminal",
+        author="naverdocker",
+        package_dir={"": "src"},
+        packages=find_packages(where="src"),
+        install_requires=[
+            "litellm",
+        ],
+        entry_point={
+            "console_scripts": [
+                "vagus=vagus.main:entry_point",
+            ],
+        },
+)
